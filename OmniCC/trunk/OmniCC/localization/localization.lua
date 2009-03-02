c8e1314ec16baf27b417676c599ea676c7f93324
j85ason <j85ason@85caf629-fd28-0410-b983-630554dc95c4>
2009-03-02 18:29:22 +0000

    Sage 3.0 is now the trunk

--- OmniCC/trunk/OmniCC/localization/localization.lua
+++
@@ -1,9 +0,0 @@
---[[
-	OmniCC Localization File
-		English (default language)
---]]
-
-OMNICC_LOCALS = {}
-local L = OMNICC_LOCALS
-L.UpgradeIncompatible = "Upgrading from an incompatible version. Default settings loaded"
-L.Updated = "Updated to v%s"
